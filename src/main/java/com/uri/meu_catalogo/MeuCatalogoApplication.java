2aa46b90eb9459ddc876ef65e7c59d3a311db28d
Henrique Jornada <henriquejornada@MacBook-Air-de-Henrique.local>
2025-05-22 21:44:34 -0300

    feat: added initial springboot project

---
+++ src/main/java/com/uri/meu_catalogo/MeuCatalogoApplication.java
@@ -0,0 +1,13 @@
+package com.uri.meu_catalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuCatalogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uri/meu_catalogo/MeuCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uri.meu_catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuCatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
