95e4fe62a5c30fc8a3d90e53e497db7b53578379
Henrique Jornada <147172577+jornadev@users.noreply.github.com>
2025-06-05 11:48:10 -0300

    feat: added models UserRepository

---
+++ src/main/java/com/uri/meucatalogo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.uri.meucatalogo.repositories;
+
+import com.uri.meucatalogo.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+}
