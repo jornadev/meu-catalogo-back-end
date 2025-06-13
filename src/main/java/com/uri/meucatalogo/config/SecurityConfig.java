8ca3d74d74c886976d2bbc4888551345547acc48
jornadev <jornadahenrique12@gmail.com>
2025-06-13 20:35:13 -0300

    refactor: update security policies

--- src/main/java/com/uri/meucatalogo/config/SecurityConfig.java
+++ src/main/java/com/uri/meucatalogo/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/api/auth/login").permitAll()
                         .requestMatchers("/api/auth/register").permitAll()
                         .anyRequest().authenticated()
